85ca8cd76c055b8ea3379946d66d48a9eaf6854f
Mstislav Zhivodkov <stevemyz@gmail.com>
2015-12-10 21:08:27 +0600

    Add phaser build

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const server = app.listen(port, () => console.log(`server listen on ${port} port
 
 app.use(express.static(path.join(__dirname, '../dist')));
 app.use(express.static(path.join(__dirname, '../assets')));
+app.use(express.static(path.join(__dirname, '../node_modules/phaser/build')));
 
 // error handlers
 app.use((error, req, res, next) => res.send(error));
